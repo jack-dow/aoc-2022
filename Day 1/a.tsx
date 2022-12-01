eeddb1daa5c732604e33c5685ec93f575795a8bc
Jack Dow <jack.dowww@gmail.com>
2022-12-01 22:36:00 +1000

    first commit

---
+++ Day 1/a.tsx
@@ -0,0 +1,6 @@
+export const x = "";
+
+const results = await Deno.readTextFile("./input.txt");
+const lines = results.split("\n");
+
+// Lost what i did because i reloaded whoops
---
+++ Template/b.tsx
@@ -0,0 +1,4 @@
+export const x = "";
+
+const results = await Deno.readTextFile("./input.txt");
+const lines = results.split("\n");
