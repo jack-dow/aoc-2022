be99578b9cad07daa74bab5637bb640a71e269a0
Jack Dow <jack.dowww@gmail.com>
2022-12-05 23:26:06 +1000

    complete day 5

---
+++ 5/b.ts
@@ -0,0 +1,44 @@
+export const x = "";
+
+const results = await Deno.readTextFile("./input.txt");
+const lines = results.split("\n\n");
+const stackInput = lines[0].split("\n").reverse();
+const moves = lines[1].split("\n");
+
+const stacks: Record<string, string[]> = {};
+
+stackInput.forEach((line, index) => {
+  if (index === 0) {
+    const stackNumbers = line.trim().split("   ");
+    stackNumbers.forEach((stackNumber) => {
+      stacks[stackNumber] = [];
+    });
+  }
+  if (index > 0) {
+    const crates = line.match(/.{1,4}/g) || [];
+
+    crates.forEach((crate, index) => {
+      if (crate.trim()) {
+        stacks[index + 1].push(crate[1]);
+      }
+    });
+  }
+});
+
+moves.forEach((move) => {
+  const movesArray = move.split(" ");
+  const amount = parseInt(movesArray[1]);
+  const from = movesArray[3];
+  const to = movesArray[5];
+
+  const crates = stacks[from].splice(-amount);
+  stacks[to].push(...crates);
+});
+
+let output = "";
+
+Object.keys(stacks).forEach((stack) => {
+  output += stacks[stack].pop();
+});
+
+console.log(output);
