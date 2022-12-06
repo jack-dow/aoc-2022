2b88fdada56393bd795ca9542238614d385ba67d
Jack Dow <jack.dowww@gmail.com>
2022-12-06 19:20:12 +1000

    complete day 6

---
+++ 6/b.ts
@@ -0,0 +1,22 @@
+export const x = "";
+
+const results = await Deno.readTextFile("./input.txt");
+const signals = results.split("\n");
+
+signals.forEach((signal) => {
+  const buffer = signal.split("");
+
+  let markerPosition = 0;
+
+  for (let i = 0; i < buffer.length - 13; i++) {
+    const marker = buffer.slice(i, i + 14).join("");
+
+    const uniqueChars = new Set(marker).size === marker.length;
+    if (uniqueChars) {
+      markerPosition = i + 14;
+      break;
+    }
+  }
+
+  console.log(markerPosition);
+});
